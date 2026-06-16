483d0d355ca21ae6d2f3fa8e7ee86a002b567f48
Harsh Singh <harsh.singh.dev07@gmail.com>
2026-06-17 00:48:13 +0530

    Add basic printf example in C

---
+++ C/print_variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // declares integer 
+    int a;
+    // assigns the value to variable
+    a=6;
+    //prints variable
+    printf("The output of this program is %d",a);
+    return 0;
+}
